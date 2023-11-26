929a0c0549b9611515b95303864416c4d88e09db
dimasb1954 <dimasbagoes1954@gmail.com>
2023-11-26 14:24:01 +0700

    main program

--- main.py
+++ main.py
@@ -5,28 +5,13 @@ import pda
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-<<<<<<< Updated upstream
-    if (len(sys.argv)>1) :
-        fileName = str(sys.argv[1])
-    else :
-        fileName = "test.html"
-
-    arr = OpenFile.ReadRawFile(fileName)
-=======
     fileName = sys.argv[2]
     filePDA = sys.argv[1]
     arr = OpenFile.readFile(fileName)
->>>>>>> Stashed changes
     arr = OpenFile.splitFile(arr)
     arr = OpenFile.handleString(arr)
     arr = OpenFile.handleComment(arr)
     arr = OpenFile.handlePetik(arr)
     arr = OpenFile.getGrammar(arr)
-<<<<<<< Updated upstream
-
-
-    print(arr)
-=======
     pda.start(filePDA)
     pda.process(arr)
->>>>>>> Stashed changes
